3b4366b4fcd8527c2d4fd78d25c326e3ee220bb4
afrazrajpoot <afrazrajpoot46@gmail.com>
2024-01-22 03:21:54 +0500

    add button to nevigate home page

--- my-app/src/app/about/about2/page.tsx
+++ my-app/src/app/about/about2/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { formData } from '@/appData/data'
+import Link from 'next/link'
 import React from 'react'
 
 const Cards = () => {
@@ -8,9 +9,9 @@ const Cards = () => {
     <>
       <section className='bg-slate-200  h-[100vh] grid grid-cols-4 place-items-center place-content-center'>
         {
-            formData.map((data)=>{
+            formData?.map((data)=>{
                 return(
-                    <div className=' bg-blue-300 p-[2vw] shadow-lg rounded-md hover:bg-blue-300 hover:opacity-25 transition  duration-[0.3s] hover:cursor-pointer '>
+                    <div className=' bg-blue-300 p-[2vw] shadow-lg rounded-md w-[200px] h-[200px]  hover:bg-blue-300 hover:opacity-25 transition  duration-[0.3s] hover:cursor-pointer '>
                         <h1>{data.name}</h1>
                         <h1>{data.type}</h1>
                         <h1>{data.placeholder}</h1>
@@ -18,7 +19,13 @@ const Cards = () => {
                 )
             })
         }
+           <Link href={"/"}>
+        <button className="bg-red-200 p-3 rounded-md shadow-lg">
+          Home page
+        </button>
+      </Link>
       </section>
+   
     </>
   )
 }
