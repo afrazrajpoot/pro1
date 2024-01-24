7ae75bb435571851b5f07dc92c6ae651a6549c7a
afrazrajpoot <afrazrajpoot46@gmail.com>
2024-01-24 20:09:03 +0500

    error handling

--- my-app/src/app/about/about2/error.tsx
+++ my-app/src/app/about/about2/error.tsx
@@ -3,7 +3,7 @@
 const error = ({error,reset}:{error:Error,reset: ()=>void}) => {
   return (
     <div>
-     <h1>about 2 error</h1>
+     <h1>about 2 error {error.message}</h1>
      <button onClick={()=>reset()} className="bg-green-500 p-3">try again</button> 
     </div>
   )
