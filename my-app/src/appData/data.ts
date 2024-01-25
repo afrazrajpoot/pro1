c51b09702d745a7fdcdcb13304c6f7adbfeba49e
afrazrajpoot <afrazrajpoot46@gmail.com>
2024-01-25 09:06:38 +0500

    25 jan practice day

--- my-app/src/appData/data.ts
+++ my-app/src/appData/data.ts
@@ -1,51 +1,19 @@
-interface Data {
-    name: string;
-    course:string
-    score:number
-  }
-  export interface formObj{
-    name?:string
-    type:string
-    placeholder?:string
-    value?:string
-  }
-export  const arr:Data[]=[
-    {
-      name:'Afraz',
-      course:"BSC",
-      score:90
-    },
-    {
-      name:'Ali',
-      course:"BSC",
-      score:9
-    },
-    {
-      name:'Musa',
-      course:"BSC",
-      score:100
-    },
-  ]
-  export const formData:formObj[] = [
-    {
-      name:"username",
-      type:"text",
-      placeholder:"Enter Username"
-    },
-    {
-      name:"password",
-      type:"password",
-      placeholder:"Enter Passord"
-    },
-    {
-      name:"email",
-      type:"email",
-      placeholder:"Enter Email Address"
-    },
-    {
-      
-      type:"submit",
-      value:"Submit"
-    },
-  ]
- 
+export const navData = [
+  {
+    title:"Home",
+    url:'/'
+  },
+  {
+    title:"About",
+    url:'/about'
+  },
+  {
+    title:"Contact",
+    url:'/contact'
+  },
+  {
+    title:"Services",
+    url:'/services'
+  },
+
+]
---
+++ my-app/src/components/Nav.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react";
+let arr1 = [5, 4, 3, 2, 1];
+const Nav = () => {
+  const [arr, setArr] = useState(arr1);
+  function updateArray() {
+    let arr2 = [...arr].sort((a, b) => a - b);
+    setArr(arr2);
+    console.log(arr2);
+  }
+  function descArray() {
+    // let arr2 = [...arr].sort((a,b)=>b-a)
+    // setArr(arr2)
+    // console.log(arr2)
+    setArr(arr1);
+  }
+  return (
+    <>
+      {[...arr].map((item, index) => {
+        return <li key={index}>{item}</li>;
+      })}
+      <button onClick={updateArray}>click me</button>
+      <button onClick={descArray}>desc</button>
+    </>
+  );
+};
+
+export default Nav;
